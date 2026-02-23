fed960b9b8884f2b497de1d713a772b34915b17d
Matveyamba <matya2303@mail.ru>
2026-02-23 20:32:02 +0300

    add method to check login button visibility on main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,13 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
+    URL = "https://selectel.ru"
+    LOGIN_BUTTON = (By.XPATH, "//a[contains(text(), 'Войти')]")
 
     def open(self):
-        super().open("https://selectel.ru")
+        super().open(self.URL)
+
+    def is_login_button_visible(self):
+        return self.find_element(self.LOGIN_BUTTON).is_displayed()
